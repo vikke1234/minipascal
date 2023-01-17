9048a88b28868acadbddd48f879f8149f6a2bdea
Viktor Horsmanheimo <viktor.horsmanheimo@helsinki.fi>
2023-01-17 17:24:55 +0200

    added basic command line parsing

--- minipascal.cpp
+++ minipascal.cpp
@@ -1,12 +1,35 @@
-#include <stdio.h>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <unistd.h>
 
-#define PROJECT_NAME "minipascal"
+const char * usage = "./minipascal filename\n";
 
 int main(int argc, char **argv) {
-    if(argc != 1) {
-        printf("%s takes no arguments.\n", argv[0]);
+    if(argc == 1) {
+        std::cout << usage;
         return 1;
     }
-    printf("This is project %s.\n", PROJECT_NAME);
-    return 0;
+
+    std::string filename;
+
+    int opt = 0;
+
+    // added for future
+    while (true) {
+        opt = getopt(argc, argv, "v");
+        if (opt == -1) {
+            break;
+        }
+
+        switch(opt) { }
+    }
+
+    if (optind >= argc) {
+        std::cout << "Expected filename";
+        std::exit(EXIT_FAILURE);
+    }
+
+    filename = argv[optind];
+    return EXIT_SUCCESS;
 }
