045d0333addc30886008e8feee0533dff651c274
Viktor Horsmanheimo <viktor.horsmanheimo@helsinki.fi>
2023-02-28 19:05:44 +0200

    Added doxygen comments for functions

--- parser.h
+++ parser.h
@@ -35,9 +35,32 @@ private:
      * Fetches a terminal, e.g. digit or identifier
      */
     std::unique_ptr<Expr> terminal();
+
+    /**
+     * Fetches the tail if it exists, if not it will return the terminal
+     *
+     * @param expr - the terminal, the left side operand of the expression that
+     *               is being built
+     */
     std::unique_ptr<Expr> term_tail(std::unique_ptr<Expr> expr);
+
+    /**
+     * Fetches a digit/identifier, also makes sure parentheses are handled.
+     */
     std::unique_ptr<Expr> factor();
+
+    /**
+     * Tries to factor the expression if needed, for example handles
+     * parentheses and operator order.
+     *
+     * @param ident - left side of the expression (e.g. 1 * 3, the `1` will be
+     * passed)
+     */
     std::unique_ptr<Expr> factor_tail(std::unique_ptr<Expr> ident);
+
+    /**
+     * Parses an expression
+     */
     std::unique_ptr<Expr> expression();
 
     std::unique_ptr<Token> match(token_type expected) {
