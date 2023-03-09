16310bfef496b986f44cecdc49022a4f3bd1bb4e
Viktor Horsmanheimo <viktor.horsmanheimo@helsinki.fi>
2023-03-09 18:00:24 +0200

    Improved symbol table and removed unnecessary structures

--- symbols.cpp
+++ symbols.cpp
@@ -1,12 +1,19 @@
 #include "symbols.h"
+#include "expr.h"
+#include <memory>
 #include <string>
 #include <unordered_map>
 #include <variant>
 
-void SymbolTable::add_symbol(std::string_view symbol) {
-    (*symbols.get())[symbol.data()] = 0;
+
+SymbolTable symbol_table;
+
+
+bool SymbolTable::add_symbol(std::string_view symbol,
+        std::unique_ptr<Literal> value) {
+    symbols[symbol.data()] = std::move(value);
 }
 
-value_type &SymbolTable::get_symbol(std::string_view str) {
-    return symbols.get()->at(str.data());
+Literal *SymbolTable::get_symbol(std::string_view str) {
+    return symbols.at(str.data()).get();
 }
--- symbols.h
+++ symbols.h
@@ -2,29 +2,21 @@
 #define SYMBOLS_H
 
 #include <string>
-#include <variant>
 #include <unordered_map>
+#include <variant>
 #include <memory>
 
-struct value {
-    union {
-        std::string str;
-        std::int64_t num;
-        bool b;
-    };
+class Literal;
 
-    enum {STR, BOOL, INT} tag;
-};
-
-using value_type = std::variant<std::string, int, bool>;
 class SymbolTable {
-    std::unique_ptr<std::unordered_map<std::string, value_type>> symbols;
-
+    std::unordered_map<std::string, std::unique_ptr<Literal>> symbols;
 
 public:
     SymbolTable() : symbols{} {}
-    void add_symbol(std::string_view symbol);
-    value_type &get_symbol(std::string_view symbol);
+    bool add_symbol(std::string_view symbol, std::unique_ptr<Literal> value);
+    Literal *get_symbol(std::string_view symbol);
 };
+
+extern SymbolTable symbol_table;
 
 #endif /* SYMBOLS_H */
